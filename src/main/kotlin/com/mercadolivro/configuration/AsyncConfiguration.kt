e394f11fe259ed9c87972c18d42b7342f0e55ad9
Jonathan Henrique <jonathan.medeiros@picpay.com>
2022-06-08 21:41:16 -0300

    Enabling async process to listeners

---
+++ src/main/kotlin/com/mercadolivro/configuration/AsyncConfiguration.kt
@@ -0,0 +1,9 @@
+package com.mercadolivro.configuration
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.annotation.EnableAsync
+
+@EnableAsync
+@Configuration
+class AsyncConfiguration {
+}
